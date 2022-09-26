c1d481ab455f6aeb0072a4c22c14f2c76feab196
Mosaddek007 <hossainmdmosaddek101@gmail.com>
2022-09-27 00:56:50 +0600

    1234

---
+++ PortfolioMH/Controllers/PortfolioController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortfolioMH.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        // GET: Portfolio
+        public ActionResult Homepage()
+        {
+            return View();
+        }
+        public ActionResult Education()
+        {
+            return View();
+        }
+        public ActionResult Projects()
+        {
+            return View();
+        }
+        public ActionResult References()
+        {
+            return View();
+        }
+    }
+}
---
+++ PortfolioMH/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PortfolioMH.MvcApplication" Language="C#" %>
